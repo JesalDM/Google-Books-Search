5eb22320aef49e18d17ea109f4a57eda30161b6a
Jesal Mehta <jesaldmehta@gmail.com>
2021-03-07 22:09:46 -0500

    Modified the Nav and styled it

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
+import "./style.css"
 
 function Nav() {
   // This allows the component to check the route any time the user uses a link to navigate.
   const location = useLocation();
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-        <a className="navbar-brand" href="#">
-            Google Books
-        </a>
+    //<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
         <ul className="nav nav-tabs">
+            <li className="navbar-brand">
+                Google Books
+            </li>
             <li className="nav-item">
                 <Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>
                 Search
@@ -21,11 +22,11 @@ function Nav() {
                 to="/saved"
                 className={location.pathname === "/saved" ? "nav-link active" : "nav-link"}
                 >
-                saved
+                Saved
                 </Link>
             </li>
         </ul>
-    </nav>
+    // </nav>
   );
 }
 
