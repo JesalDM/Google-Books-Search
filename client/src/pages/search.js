50a5241fe0c3b234c6b4d75ee6fe792f98badee7
Jesal Mehta <jesaldmehta@gmail.com>
2021-03-07 22:15:05 -0500

    Created Search page that includes the Search form as well as the Search results

--- client/src/pages/search.js
+++ client/src/pages/search.js
@@ -1,8 +1,57 @@
-import React from 'react';
+import React, { useState} from "react";
+import { Input, FormBtn } from "../components/Form";
+import {Container, Row} from "react-bootstrap";
+import API from "../utils/API";
+import SearchResultContainer from "../components/SearchResultContainer.js";
+
 
 function Search(){
+ const [books, setBooks] = useState([])
+ const [formObject, setFormObject] = useState({})
+      
+// Handles updating component state when the user types into the input field
+  function handleInputChange(event) {
+    const { name, value } = event.target;
+    setFormObject({...formObject, [name]: value})
+  }; 
+
+// When the form is submitted, use the API.saveBook method to save the book data
+  // Then reload books from the database
+  function handleFormSubmit(event) {
+    event.preventDefault();
+    if (formObject.title) {
+      API.getBooks(formObject.title)
+       .then(res =>{console.log("handleFormSubmit", res); setBooks(res.data.items)})
+        .catch(err => console.log(err));
+    }
+  };
+      
     return(
-        <p>Search</p>
+        <div>
+        <Container>
+            <Row>
+                <h5>Book Search</h5>
+            </Row>
+            <Row>
+                <form>
+                    <Input
+                        onChange={handleInputChange}
+                        name="title"
+                        placeholder="Title (required)"
+                    />
+                    <FormBtn
+                        disabled={!(formObject.title)}
+                        onClick={handleFormSubmit}
+                    >
+                        Search
+                    </FormBtn>
+                </form>
+            </Row>
+        </Container>
+        <hr/>
+        <SearchResultContainer items={books} />
+
+    </div>
     )    
 };
 
